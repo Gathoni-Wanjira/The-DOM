84ee88ff4b9be3508f5e41c326df5c3bc04e9f6a
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-08 16:10:44 +0300

    Remove Elements

--- dom.js
+++ dom.js
@@ -1,21 +1,30 @@
 // Adding elements to the page
 const body = document.body
 // body.append("Hello World");...works because the text is a string.
-const div = document.createElement("div")
+// const div = document.createElement("div")
 // body.append(div) ..... cannot work since we need to use div is an element
 // div.innerText = "Hello Everyone"
 // div.textContent = "Hey Guys"
 
-div.innerHTML = "<strong>It should be in bold</strong>"
+// div.innerHTML = "<strong>It should be in bold</strong>"
 // Always use .innerHTML to access elements within texts.
 // Instead of using .innerHTML you can create an element  with text as follows
 
-const strong = document.createElement('strong');
-strong.innerText="Its nap time";
-div.append(strong)
+// const strong = document.createElement('strong');
+// strong.innerText="Its nap time";
+// div.append(strong)
+
+const div = document.querySelector("div")
+const spanHi = document.querySelector('#hi')
+const spanBye = document.querySelector('#bye')
+
+spanHi.remove()
+body.append(spanHi)
 
 
-body.appendChild(div)
+// body.appendChild(div)
+
+
 
 // You cannot use append.child while adding strings or other data types.... 
 // append.child is only used when you want to append elements like divs and span and tags.
